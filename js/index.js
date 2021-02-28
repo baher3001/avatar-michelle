cc3d95af1d1efc8bec3f4751ba2584763922b66a
Baher902 <Baheratef10@gmail.com>
2021-02-28 15:21:50 +0200

    website

---
+++ js/index.js
@@ -0,0 +1,13 @@
+let icons =  document.getElementById('icons');
+let Btn = document.querySelector('.img2')
+let cancel = document.querySelector('.c0')
+
+Btn.onclick = function()
+{
+    icons.style.display="block";
+}
+cancel.onclick = function()
+{
+    icons.style.display="none";
+
+}
